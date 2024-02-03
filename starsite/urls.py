c3299b1fca7b43502c6d7d7bc79dff19aa9a8cda
REBECCA YU <rebeccayu2015@gmail.com>
2024-02-03 14:55:09 -0500

    Set up static files

--- starsite/urls.py
+++ starsite/urls.py
@@ -22,6 +22,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
-    # path('', include('users.urls')),
+    path('', include('users.urls')),
     path('', include('django.contrib.auth.urls')),
 ]
