87c9ec1fd7e0644e5d84ca9f97eb9a92a5a69e4d
Matthew Lee <matthew.y.lee101@gmail.com>
2024-02-04 01:50:08 -0500

    Fix models.py and views.py for profile database

--- core/models.py
+++ core/models.py
@@ -12,12 +12,13 @@ with open('static/json/constellations.json') as json_star:
         image_url = ""
         found = 0
         starDict2[name] = [found, image_url]
+jsonConstells = json.dumps(starDict2)
 
 class ProfileConstellation(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name = 'curr_prof')
-    image_id = models.ImageField(upload_to="media/", height_field=None, width_field=None, max_length=100)
+    image_src = models.ImageField(upload_to="media/", height_field=None, width_field=None, max_length=100)
     image_name = models.CharField(max_length = 100)
-    found_dict = dict(starDict2)
+    found_dict = models.CharField(max_length = 10000, default = 'none')
 
 class Image(models.Model):
     image_id = models.AutoField(primary_key=True)
