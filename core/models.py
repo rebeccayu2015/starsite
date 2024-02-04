d229d03818693daabd4dc0a44206fc19228fec4f
REBECCA YU <rebeccayu2015@gmail.com>
2024-02-04 01:51:17 -0500

    Added submit/back buttons

--- core/models.py
+++ core/models.py
@@ -15,7 +15,7 @@ with open('static/json/constellations.json') as json_star:
 
 class ProfileConstellation(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name = 'curr_prof')
-    image_id = models.ImageField(upload_to="media/", height_field=None, width_field=None, max_length=100)
+    image_id = models.ImageField(upload_to="media/images", height_field=None, width_field=None, max_length=100)
     image_name = models.CharField(max_length = 100)
     found_dict = dict(starDict2)
 
@@ -25,5 +25,5 @@ class Image(models.Model):
     time_submitted = models.DateTimeField(auto_now_add=True)
     constellation_name = models.CharField(max_length=100)
     notes = models.TextField(max_length=1000)
-    image = models.ImageField(upload_to='images/')
+    image = models.ImageField(upload_to='media/images/')
     score = models.IntegerField()
